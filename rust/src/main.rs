4940634344fea4a763f28a0a8be7e78a4fa5ac84
Ezekiel Kigbo <theatlasroom@gmail.com>
2018-10-05 02:22:01 +0200

    solve 2015_1 in rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,8 @@
+mod utils;
+mod aoc2015_1;
+
 fn main() {
-    println!("Hello, world!");
+    println!("2015");
+    println!("======================================");
+    println!("Puzzle 1 => {}", aoc2015_1::solve());
 }
