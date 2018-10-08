258cd94f9eb238a841727a88646c1947b0476c7c
Ezekiel Kigbo <theatlasroom@gmail.com>
2018-10-09 00:55:18 +0200

    finished day 3 - js

--- js/2015_3.test.js
+++ js/2015_3.test.js
@@ -1,5 +1,5 @@
 const deliveries = require('./2015_3')
-const data = [['>', -1], ['^>v<', 5], ['^v^v^v^v^v', 2]]
+const data = [['>', 2], ['^>v<', 4], ['^v^v^v^v^v', 2]]
 data.forEach(([testCase, result]) => {
   test(`${testCase} returns ${result}`, () => {
     expect(deliveries(testCase)).toEqual(result)
