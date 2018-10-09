a635dc846d21e792087db3f979c38f4ebae4a3ba
Ezekiel Kigbo <theatlasroom@gmail.com>
2018-10-09 14:22:34 +0200

    finished day 3, part 2

--- js/2015_3.test.js
+++ js/2015_3.test.js
@@ -5,3 +5,11 @@ data.forEach(([testCase, result]) => {
     expect(deliveries(testCase)).toEqual(result)
   })
 })
+
+const deliveriesPartTwo = require('./2015_3_2')
+const dataPartTwo = [['^v', 3], ['^>v<', 3], ['^v^v^v^v^v', 11]]
+dataPartTwo.forEach(([testCase, result]) => {
+  test(`${testCase} returns ${result}`, () => {
+    expect(deliveriesPartTwo(testCase)).toEqual(result)
+  })
+})
---
+++ js/2015_3_2.js
@@ -0,0 +1,90 @@
+'use strict'
+/* Santa is delivering presents to an infinite two-dimensional grid of houses.
+
+   He begins by delivering a present to the house at his starting location, and then an elf at the North Pole calls him via radio and tells him where to move next. Moves are always exactly one house to the north (^), south (v), east (>), or west (<). After each move, he delivers another present to the house at his new location.
+
+   However, the elf back at the north pole has had a little too much eggnog, and so his directions are a little off, and Santa ends up visiting some houses more than once. How many houses receive at least one present?
+
+   For example:
+
+   > delivers presents to 2 houses: one at the starting location, and one to the east.
+   ^>v< delivers presents to 4 houses in a square, including twice to the house at his starting/ending location.
+   ^v^v^v^v^v delivers a bunch of presents to some very lucky children at only 2 houses. */
+
+/* naive: use a regex to scan the input for all occurrences of the strings we want to find */
+/* "better" create a parser */
+
+const utils = require('../utils')
+const Actions = {
+  UP: '^',
+  DOWN: 'v',
+  LEFT: '<',
+  RIGHT: '>',
+}
+
+const move = (currentPosition, delta) => ({
+  x: delta.x + currentPosition.x,
+  y: delta.y + currentPosition.y,
+})
+const generateKey = pt => `x${pt.x}y${pt.y}`
+
+function matchDirection(action = null) {
+  switch (action) {
+    case Actions.UP:
+      return { x: 0, y: -1 }
+    case Actions.DOWN:
+      return { x: 0, y: 1 }
+    case Actions.LEFT:
+      return { x: -1, y: 0 }
+    case Actions.RIGHT:
+      return { x: 1, y: 0 }
+    default:
+      return { x: 0, y: 0 }
+  }
+}
+
+function nextHouse(currentPosition, action) {
+  const delta = matchDirection(action)
+  return move(currentPosition, delta)
+}
+
+function followDirection(directions) {
+  const houses = new Set()
+  let cursor = 0
+  const santas = [{ x: 0, y: 0 }, { x: 0, y: 0 }]
+
+  let currentSanta = 0
+
+  // add the starting points
+  for (let index = 0; index < santas.length; index++)
+    houses.add(generateKey(santas[index]))
+  // move across the input and calculate the next position
+  // the next position will be used for a unique key in the set
+  while (cursor < directions.length) {
+    const nextPosition = nextHouse(santas[currentSanta], directions[cursor])
+    santas[currentSanta] = nextPosition
+    houses.add(generateKey(nextPosition))
+    cursor++
+    currentSanta = Number(!currentSanta)
+  }
+
+  return houses.size
+}
+
+function calculateDeliveries(directions) {
+  return followDirection(directions)
+}
+
+const args = process.argv
+
+if (args.length > 2 && args[2]) {
+  const file = args[2]
+  console.log(`Reading input file ${file}`)
+  utils
+    .readData(file)
+    .then(calculateDeliveries)
+    .then(houses => console.log(`Santa delivered to ${houses} unique houses`))
+    .catch(console.error)
+}
+
+module.exports = calculateDeliveries
