d6cd4d1667c02e5d039a6e452caf0c9612c020e2
Ezekiel Kigbo <theatlasroom@gmail.com>
2018-10-11 23:43:11 +0200

    finished rust day 2 part 2

--- rust/src/aoc2015_2.rs
+++ rust/src/aoc2015_2.rs
@@ -3,19 +3,19 @@ use utils;
 const SEPARATOR: char = 'x';
 
 #[derive(Debug, PartialEq)]
-struct BoxDimensions {
+struct SantaPresent {
     width: i32,
     height: i32,
     length: i32,
 }
 
-fn parse_line(line: &str, separator: char) -> BoxDimensions {
+fn parse_line(line: &str, separator: char) -> SantaPresent {
     // TODO: LOOOOOL this seems bad, very very bad
     let mut chars = line.split(separator);
     let length = chars.next().unwrap().parse::<i32>().unwrap();
     let width = chars.next().unwrap().parse::<i32>().unwrap();
     let height = chars.next().unwrap().parse::<i32>().unwrap();
-    BoxDimensions {
+    SantaPresent {
         length,
         width,
         height,
@@ -26,7 +26,7 @@ fn parse_line(line: &str, separator: char) -> BoxDimensions {
 fn it_will_correctly_parse_a_line_of_input() {
     assert_eq!(
         parse_line("29x13x26", SEPARATOR),
-        BoxDimensions {
+        SantaPresent {
             width: 13,
             length: 29,
             height: 26
@@ -35,7 +35,7 @@ fn it_will_correctly_parse_a_line_of_input() {
 }
 
 // TODO: should be implemented for the struct...
-fn compute_surface_area(cuboid: &BoxDimensions) -> i32 {
+fn compute_surface_area(cuboid: &SantaPresent) -> i32 {
     // compute all the faces
     // sort to find the smallest face
     let mut faces = vec![
@@ -50,13 +50,13 @@ fn compute_surface_area(cuboid: &BoxDimensions) -> i32 {
 
 #[test]
 fn it_will_compute_surface_area() {
-    let b = BoxDimensions {
+    let b = SantaPresent {
         length: 2,
         width: 3,
         height: 4,
     };
     assert_eq!(compute_surface_area(&b), 58);
-    let b2 = BoxDimensions {
+    let b2 = SantaPresent {
         length: 1,
         width: 1,
         height: 10,
@@ -65,7 +65,7 @@ fn it_will_compute_surface_area() {
 }
 
 // TODO: should be implemented for the struct...
-fn compute_bow_ribbon_length(cuboid: &BoxDimensions) -> i32 {
+fn compute_bow_ribbon_length(cuboid: &SantaPresent) -> i32 {
     // compute all the faces
     // sort to find the smallest face
     let mut faces = vec![cuboid.length, cuboid.width, cuboid.height];
@@ -75,13 +75,13 @@ fn compute_bow_ribbon_length(cuboid: &BoxDimensions) -> i32 {
 
 #[test]
 fn it_will_compute_bow_ribbon_length_required() {
-    let b = BoxDimensions {
+    let b = SantaPresent {
         length: 2,
         width: 3,
         height: 4,
     };
     assert_eq!(compute_bow_ribbon_length(&b), 10);
-    let b2 = BoxDimensions {
+    let b2 = SantaPresent {
         length: 1,
         width: 1,
         height: 10,
@@ -90,7 +90,7 @@ fn it_will_compute_bow_ribbon_length_required() {
 }
 
 // TODO: should be implemented for the struct...
-fn compute_wrapping_ribbon_length(cuboid: &BoxDimensions) -> i32 {
+fn compute_wrapping_ribbon_length(cuboid: &SantaPresent) -> i32 {
     // compute all the faces
     // sort to find the smallest face
     let mut faces = vec![cuboid.length, cuboid.width, cuboid.height];
@@ -100,13 +100,13 @@ fn compute_wrapping_ribbon_length(cuboid: &BoxDimensions) -> i32 {
 
 #[test]
 fn it_will_compute_wrapping_ribbon_length_required() {
-    let b = BoxDimensions {
+    let b = SantaPresent {
         length: 2,
         width: 3,
         height: 4,
     };
     assert_eq!(compute_wrapping_ribbon_length(&b), 24);
-    let b2 = BoxDimensions {
+    let b2 = SantaPresent {
         length: 1,
         width: 1,
         height: 10,
@@ -124,7 +124,22 @@ fn calculate_wrapping_paper(_data: &str) -> i32 {
     sum
 }
 
-pub fn solve() -> i32 {
+fn calculate_total_ribbon_needed(_data: &str) -> i32 {
+    let lines = utils::read_file_by_lines("../data/2015_2.txt");
+    let mut sum: i32 = 0;
+    for line in lines {
+        let b = parse_line(&line, SEPARATOR);
+        sum += compute_wrapping_ribbon_length(&b) + compute_bow_ribbon_length(&b);
+    }
+    sum
+}
+
+pub fn solve() -> String {
     let data = utils::read_file("../data/2015_1.txt");
-    calculate_wrapping_paper(&data)
+    let wrapping_paper = calculate_wrapping_paper(&data);
+    let ribbon = calculate_total_ribbon_needed(&data);
+    format!(
+        "{:?} square feet of wrapping paper and {:?} feet of ribbon",
+        wrapping_paper, ribbon
+    )
 }
