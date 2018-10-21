6bab16dfee8303ff5e92e0885c4a03ad457a93a4
Ezekiel Kigbo <theatlasroom@gmail.com>
2018-10-21 10:17:23 +0200

    JS - day 5: refactor for part 2

--- js/2015_5.js
+++ js/2015_5.js
@@ -11,6 +11,9 @@
 const utils = require('../utils')
 const args = process.argv
 
+const PARSER_ONE = 'PARSER_ONE'
+const PARSER_TWO = 'PARSER_TWO'
+
 // const threeVowels = str, pattern
 
 // isNiceString = string => bool
@@ -29,17 +32,35 @@ function isNiceString(str = '') {
   )
 }
 
+// isNiceString = string => bool
+function improvedNiceString(str = '') {
+  if (!str || !str.length) return false
+  const flags = 'gi'
+  // a letter, repeated, with a different letter in between
+  // ie xyx
+  const REPEATED_LETTER_SEPARATED_BY_ANOTHER = new RegExp('([A-Za-z]).\\1')
+  // two letter sequence, repeated
+  // ie xyfkdkelxy
+  const REPEATED_TWO_LETTERS = new RegExp('([A-Za-z]{2}).*\\1')
+  return (
+    REPEATED_TWO_LETTERS.test(str) &&
+    REPEATED_LETTER_SEPARATED_BY_ANOTHER.test(str)
+  )
+}
+
 // parseInput = Array => Number
-function parseInput(dataArr) {
+function parseInput(dataArr, parser) {
+  const parsingFunction =
+    parser == 'PARSER_ONE' ? isNiceString : improvedNiceString
   return dataArr
-    .map(isNiceString)
+    .map(parsingFunction)
     .reduce((acc, next) => (!!next ? acc + 1 : acc), 0)
 }
 
-function countNiceStrings(data = []) {
+function countNiceStrings(data = [], parser = PARSER_ONE) {
   const dataArr = Array.isArray(data) ? data : [data]
   if (!data || dataArr.length < 1) return 0
-  return parseInput(data)
+  return parseInput(data, parser)
 }
 
 if (args.length > 2 && args[2]) {
@@ -47,14 +68,20 @@ if (args.length > 2 && args[2]) {
   console.log(`Reading input file ${file}`)
   utils
     .readData(file)
-    .then(inputData => countNiceStrings(inputData.split('\n')))
-    .then(totalNiceStrings =>
-      console.log(`There are ${totalNiceStrings} nice strings in the text`)
-    )
+    .then(inputData => {
+      const p1 = countNiceStrings(inputData.split('\n'), PARSER_ONE)
+      const p2 = countNiceStrings(inputData.split('\n'), PARSER_TWO)
+      return Promise.all([p1, p2])
+    })
+    .then(([part1, part2]) => {
+      console.log(`Part 1: there are ${part1} nice strings in the text`)
+      console.log(`Part 2: there are ${part2} nice strings in the text`)
+    })
     .catch(console.error)
 }
 
 module.exports = {
   countNiceStrings,
   isNiceString,
+  improvedNiceString,
 }
