b96c2bc6d827b511140a8622b7932db822b4dc10
Ezekiel Kigbo <theatlasroom@gmail.com>
2018-10-21 10:17:23 +0200

    JS - day 5: refactor for part 2

--- js/2015_5.test.js
+++ js/2015_5.test.js
@@ -1,18 +1,30 @@
-const { isNiceString } = require('./2015_5')
+const { isNiceString, improvedNiceString } = require('./2015_5')
 // ugknbfddgicrmopn is nice because it has at least three vowels (u...i...o...), a double letter (...dd...), and none of the disallowed substrings.
 // aaa is nice because it has at least three vowels and a double letter, even though the letters used by different rules overlap.
 // jchzalrnumimnmhp is naughty because it has no double letter.
 // haegwjzuvuyypxyu is naughty because it contains the string xy.
 // dvszwmarrgswjxmb is naughty because it contains only one vowel.
-const data = [
+const partOneData = [
   ['ugknbfddgicrmopn', true],
   ['aaa', true],
   ['jchzalrnumimnmhp', false],
   ['haegwjzuvuyypxyu', false],
   ['dvszwmarrgswjxmb', false],
 ]
-data.forEach(([testCase, result]) => {
+partOneData.forEach(([testCase, result]) => {
   test(`${testCase} returns ${result}`, () => {
     expect(isNiceString(testCase)).toEqual(result)
+  })
+})
+
+const partTwoData = [
+  ['qjhvhtzxzqqjkmpb', true],
+  ['xxyxx', true],
+  ['uurcxstgmygtbstg', false],
+  ['ieodomkazucvgmuy', false],
+]
+partTwoData.forEach(([testCase, result]) => {
+  test(`${testCase} returns ${result}`, () => {
+    expect(improvedNiceString(testCase)).toEqual(result)
   })
 })
