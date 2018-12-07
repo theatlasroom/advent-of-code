8f94cbbd5ef46c34b3783527c5790e70ce3061b9
Ezekiel Kigbo <theatlasroom@gmail.com>
2018-12-07 13:04:53 +1100

    dirty: rust day 2

--- rust/src/aoc2018_2.rs
+++ rust/src/aoc2018_2.rs
@@ -9,24 +9,18 @@ use utils;
 // abcdee contains two e.
 // ababab contains three a and three b, but it only counts once.
 
-fn repeated_character(s: &str, max_repeats: i32) -> bool {
+// Todo: should return a tuple (repeat_2, repeat_3)
+fn repeated_character(s: &str, c: &str, max_repeats: i32) -> bool {
   let mut has_repeat = false;
-  let mut set: HashMap<char, i32> = HashMap::new();
-  for c in s.chars() {
-    if !set.contains_key(&c) {
-      set.insert(c, 1);
-    } else {
-      let mut count = match set.get(&c) {
-        Some(&v) => v,
-        _ => 0,
-      };
+  let mut count = 0;
+  for curr in s.chars() {
+    if curr.to_string() == c {
       count += 1;
-      if count >= max_repeats {
-        has_repeat = true;
-        break;
-      } else {
-        set.insert(c, count);
-      }
+    }
+    if count == max_repeats {
+      has_repeat = true;
+    } else {
+      has_repeat = false;
     }
   }
   has_repeat
@@ -35,27 +29,27 @@ fn repeated_character(s: &str, max_repeats: i32) -> bool {
 #[test]
 fn will_check_for_repetitions_of_a_character() {
   let repeat_2 = vec![
-    ("abcdef", false),
-    ("bababc", true),
-    ("abbcde", true),
-    ("aabcdd", true),
+    ("abcdef", "a", false),
+    ("bababc", "b", true),
+    ("abbcde", "b", true),
+    ("aabcdd", "d", true),
   ];
   let repeat_3 = vec![
-    ("abcdef", false),
-    ("bababc", true),
-    ("abbcde", false),
-    ("aabcdd", false),
+    ("abcdef", "a", false),
+    ("bababc", "b", true),
+    ("abbcde", "b", false),
+    ("aabcdd", "d", false),
   ];
 
   for i in repeat_2 {
-    let (q, a) = i;
-    let res = repeated_character(q, 2);
+    let (q, c, a) = i;
+    let res = repeated_character(q, c, 2);
     assert_eq!(res, a);
   }
 
   for i in repeat_3 {
-    let (q, a) = i;
-    let res = repeated_character(q, 3);
+    let (q, c, a) = i;
+    let res = repeated_character(q, c, 3);
     assert_eq!(res, a);
   }
 }
@@ -64,14 +58,24 @@ fn calculate_checksum(lines: &str) -> i32 {
   let mut repeat_2 = 0;
   let mut repeat_3 = 0;
   for line in lines.split("\n") {
-    if repeated_character(&line, 2) {
-      if repeated_character(&line, 3) {
-        repeat_3 += 1
-      } else {
-        repeat_2 += 1
+    let mut has_2 = false;
+    let mut has_3 = false;
+    for c in line.trim().chars() {
+      if repeated_character(&line, &c.to_string(), 2) {
+        has_2 = true;
+      }
+      if repeated_character(&line, &c.to_string(), 3) {
+        has_3 = true;
       }
     }
+    if has_2 {
+      repeat_2 += 1;
+    }
+    if has_3 {
+      repeat_3 += 1;
+    }
   }
+  println!("rep2: {:?} rep3: {:?}", repeat_2, repeat_3);
   repeat_2 * repeat_3
 }
 
