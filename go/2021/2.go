0655812f79157a37b7893852a9011791ed2a01ac
Ezekiel <theatlasroom@gmail.com>
2021-12-03 02:19:06 +1100

    Finished day 2, part 2

--- go/2021/2.go
+++ go/2021/2.go
@@ -43,10 +43,48 @@ After following these instructions, you would have a horizontal position of 15 a
 
 Calculate the horizontal position and depth you would have after following the planned course. What do you get if you multiply your final horizontal position by your final depth?
 
+--- Part Two ---
+
+Based on your calculations, the planned course doesn't seem to make any sense. You find the submarine manual and discover that the process is actually slightly more complicated.
+
+In addition to horizontal position and depth, you'll also need to track a third value, aim, which also starts at 0. The commands also mean something entirely different than you first thought:
+
+    down X increases your aim by X units.
+    up X decreases your aim by X units.
+    forward X does two things:
+        It increases your horizontal position by X units.
+        It increases your depth by your aim multiplied by X.
+
+Again note that since you're on a submarine, down and up do the opposite of what you might expect: "down" means aiming in the positive direction.
+
+Now, the above example does something different:
+
+    forward 5 adds 5 to your horizontal position, a total of 5. Because your aim is 0, your depth does not change.
+    down 5 adds 5 to your aim, resulting in a value of 5.
+    forward 8 adds 8 to your horizontal position, a total of 13. Because your aim is 5, your depth increases by 8*5=40.
+    up 3 decreases your aim by 3, resulting in a value of 2.
+    down 8 adds 8 to your aim, resulting in a value of 10.
+    forward 2 adds 2 to your horizontal position, a total of 15. Because your aim is 10, your depth increases by 2*10=20 to a total of 60.
+
+After following these new instructions, you would have a horizontal position of 15 and a depth of 60. (Multiplying these produces 900.)
+
+Using this new interpretation of the commands, calculate the horizontal position and depth you would have after following the planned course. What do you get if you multiply your final horizontal position by your final depth?
+
 */
 
 type vector struct {
 	XPos, ZPos int
+}
+
+type aimVector struct {
+	XPos, ZPos, Aim int
+}
+
+type positionable interface {
+	Forward(int)
+	Up(int)
+	Down(int)
+	Position() int
 }
 
 type direction int
@@ -78,6 +116,23 @@ func (v vector) Position() int {
 	return v.ZPos * v.XPos
 }
 
+func (v *aimVector) Forward(delta int) {
+	v.XPos += delta
+	v.ZPos += delta * v.Aim
+}
+
+func (v *aimVector) Up(delta int) {
+	v.Aim -= delta
+}
+
+func (v *aimVector) Down(delta int) {
+	v.Aim += delta
+}
+
+func (v aimVector) Position() int {
+	return v.ZPos * v.XPos
+}
+
 func parseInstruction(str string) instruction {
 	strArr := s.Split(str, " ")
 	var d direction
@@ -100,22 +155,31 @@ func parseInstruction(str string) instruction {
 	}
 }
 
-func part1(data []string) {
-	var submarine vector
-
+func computePosition(sub positionable, data []string) int {
 	for _, line := range data {
 		i := parseInstruction(line)
 		switch i.Direction {
 		case DIR_DOWN:
-			submarine.Down(i.Delta)
+			sub.Down(i.Delta)
 		case DIR_UP:
-			submarine.Up(i.Delta)
+			sub.Up(i.Delta)
 		default:
-			submarine.Forward(i.Delta)
+			sub.Forward(i.Delta)
 		}
 	}
+	return sub.Position()
+}
 
-	fmt.Printf("Part 1: Final position %d\n", submarine.Position())
+func part2(data []string) {
+	var submarine aimVector
+	pos := computePosition(&submarine, data)
+	fmt.Printf("Part 2: Final position %d\n", pos)
+}
+
+func part1(data []string) {
+	var submarine vector
+	pos := computePosition(&submarine, data)
+	fmt.Printf("Part 1: Final position %d\n", pos)
 }
 
 func main() {
@@ -126,4 +190,5 @@ func main() {
 	input := utils.LoadData("2.txt")
 
 	part1(input)
+	part2(input)
 }
